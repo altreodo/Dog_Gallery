65ff58ed2402faf43c25c595a6ef612001af44be
altreodo <deepak.maurya2020@vitbhopal.ac.in>
2021-09-19 21:32:28 +0530

    First commit

---
+++ js/doggallery.js
@@ -0,0 +1,47 @@
+function om(data){
+    if(data.message.length!=0){
+        $('#br').remove();
+        var sp=' <select  id="br" name="dogs" >';
+            for(let g=0;g<data.message.length;g++){
+                sp+='<option value="'+data.message[g]+'">'+data.message[g]+'</option>';
+            }
+            $('#in').append(sp);
+    }else{
+        $('#br').remove();
+    }
+}
+function dom(){
+   var op=$('#breed').val();
+   $.ajax({
+     url:'https://dog.ceo/api/breed/'+op+'/list',
+     method:'GET',
+     success:om
+ })
+}
+function display(data){
+    console.log(data.message);
+    for(let y=0;y<data.message.length;y++){
+    var sp='<img src="'+data.message[y]+'">';
+    $('#out').append(sp);
+    }
+}
+function show(){
+$('#out').remove();
+$('body').append(' <div id="out"></div>');
+ var val=$('#breed').val();
+ var k=$('#br').val();
+ console.log(k);
+ if(k==undefined){
+ $.ajax({
+  url:'https://dog.ceo/api/breed/'+val+'/images',
+  method:'GET',
+  success:display
+ });
+}else{
+    $.ajax({
+  url:'https://dog.ceo/api/breed/'+val+'/'+k+'/images',
+  method:'GET',
+  success:display
+ });
+}
+}
